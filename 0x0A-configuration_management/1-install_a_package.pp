7628bb9e51dd355c453de9357e5e0f485292b907
Radwa Khaled <131785821+Radwa-Khaled@users.noreply.github.com>
2024-02-20 03:28:41 +0200

    1-install_a_package.pp

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# 1-install_a_package.pp
+
+# Install the flask package from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+}
+# Install the werkzueg package
+package { 'Werkzeug':
+  ensure   => '2.2.2',
+  provider => 'pip3',
+}
