503eb91652332d4447967a868e0e27423b45166c
Radwa Khaled <131785821+Radwa-Khaled@users.noreply.github.com>
2024-02-21 02:38:17 +0200

    1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# This Puppet manifest increases the hard and soft file limits for the user 'holberton' in the /etc/security/limits.conf file.
+
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
